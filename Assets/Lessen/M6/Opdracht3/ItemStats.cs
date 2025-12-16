fef350c88a23ad851b0d2d8e1b4d98069b1ffe27
Jaysen <jaysendaemon@gmail.com>
2025-12-16 14:20:32 +0100

    opdracht 3

---
+++ Assets/Lessen/M6/Opdracht3/ItemStats.cs
@@ -0,0 +1,18 @@
+using System;
+using UnityEngine;
+
+[Serializable] public struct ItemStats
+{
+    public float damage;
+    public float defense;
+    public float weight;
+
+    // Constructor (optioneel maar handig)
+    public ItemStats(float damage, float defense, float weight)
+    {
+        this.damage = damage;
+        this.defense = defense;
+        this.weight = weight;
+    }
+}
+
